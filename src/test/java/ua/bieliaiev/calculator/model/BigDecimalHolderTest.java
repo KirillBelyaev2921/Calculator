42285624b3e5f045832f088bc920faa9c5279888
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2023-12-31 19:22:50 +0200

    Add test for improper string value passed in BigDecimalHolder.

--- src/test/java/ua/bieliaiev/calculator/model/BigDecimalHolderTest.java
+++ src/test/java/ua/bieliaiev/calculator/model/BigDecimalHolderTest.java
@@ -4,18 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BigDecimalHolderTest {
 
 	@Test
 	void getZeroFromIntegerString() {
 		BigDecimalHolder number = new BigDecimalHolder("0");
+
 		assertThat(number.getDouble(), equalTo(0.0));
 	}
 
 	@Test
 	void getZeroFromDoubleString() {
 		BigDecimalHolder number = new BigDecimalHolder("0.0");
+
 		assertThat(number.getDouble(), equalTo(0.0));
 	}
 
@@ -23,6 +26,17 @@ class BigDecimalHolderTest {
 	void getNumberFromNumberString() {
 		double value = 1.1;
 		BigDecimalHolder number = new BigDecimalHolder("" + value);
+
 		assertThat(number.getDouble(), equalTo(value));
 	}
+
+	@Test
+	void getExceptionWhenImproperStringParsed() {
+		String improperValue = "123a";
+
+		assertThrows(NumberFormatException.class,
+				() -> new BigDecimalHolder(improperValue));
+
+	}
+
 }
