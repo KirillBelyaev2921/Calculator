6dcbaba64337a8ddc13f24c457e840f049003cdb
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2023-12-31 19:38:45 +0200

    Change interface that it accepts expression when parsing it.

--- src/main/java/ua/bieliaiev/calculator/model/ExpressionParser.java
+++ src/main/java/ua/bieliaiev/calculator/model/ExpressionParser.java
@@ -1,7 +1,7 @@
 package ua.bieliaiev.calculator.model;
 
 public interface ExpressionParser<T, U> {
-	T parseExpression();
+	T parseExpression(U expression);
 	boolean isExpressionValid();
 	U getExpression();
 }
