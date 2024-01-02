d77ee4af58480787a5597540d66d852c202942e9
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2024-01-02 15:51:13 +0200

    Refactor code.

--- src/main/java/ua/bieliaiev/calculator/model/rpn/ExpressionParserRPN.java
+++ src/main/java/ua/bieliaiev/calculator/model/rpn/ExpressionParserRPN.java
@@ -12,40 +12,54 @@ import java.util.regex.Pattern;
  * Expression parser, based on Reverse polish notation.
  * The result is represented by String value with coma delimiter.
  * Example: for expression '5*2+1', the result will be: '5,2,*,1,+'.
- *
  */
 public class ExpressionParserRPN implements ExpressionParser<String, String> {
 
 	private String expression;
+	Deque<String> outputQueue = new LinkedList<>();
+	Deque<String> operatorStack = new LinkedList<>();
 	private static final Pattern pattern =
 			Pattern.compile("([0-9]+)([-+*/]?)");
 
-
 	@Override
 	public String parseExpression(String expression) {
+		removeSpacesAndInitiateExpression(expression);
+		iterateExpression();
+		addTrailingOperatorsToOutput();
+		return String.join(",", outputQueue);
+	}
+
+	private void removeSpacesAndInitiateExpression(String expression) {
 		Objects.requireNonNull(expression);
 		this.expression = expression.replace(" ", "");
+	}
 
+	private void iterateExpression() {
 		Matcher m = pattern.matcher(this.expression);
 
-		Deque<String> outputQueue = new LinkedList<>();
-		Deque<String> operatorStack = new LinkedList<>();
-
 		while (m.find()) {
-			String number = m.group(1);
-			if (!number.equals("")) {
-				outputQueue.add(number);
-			}
-			String operator = m.group(2);
-			if (!operator.equals("")) {
-				operatorStack.push(operator);
-			}
+			addNumberToOutputQueue(m.group(1));
+			addOperatorToOperatorStack(m.group(2));
 		}
+	}
 
+	private void addOperatorToOperatorStack(String operator) {
+		if (!operator.equals("")) {
+			operatorStack.push(operator);
+		}
+	}
+
+	private void addNumberToOutputQueue(String number) {
+		if (!number.equals("")) {
+			outputQueue.add(number);
+		}
+	}
+
+	private void addTrailingOperatorsToOutput() {
 		while (!operatorStack.isEmpty()) {
 			outputQueue.add(operatorStack.pop());
 		}
 
-		return String.join(",", outputQueue);
 	}
+
 }
