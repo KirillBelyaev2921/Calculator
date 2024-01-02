af1dbf60a159fcce6bdf975ba7d49d41820f4419
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2024-01-02 15:42:26 +0200

    Remove unnecessarily methods.

--- src/main/java/ua/bieliaiev/calculator/model/ExpressionParser.java
+++ src/main/java/ua/bieliaiev/calculator/model/ExpressionParser.java
@@ -1,7 +1,5 @@
 package ua.bieliaiev.calculator.model;
 
 public interface ExpressionParser<T, U> {
-	T parseExpression();
-	boolean isExpressionValid();
-	U getExpression();
+	T parseExpression(U expression);
 }
